6e809b78d3b52c14abbb8b9e1563cbc24652b05a
Tony Nguyen <anhhao1999@gmail.com>
2019-07-07 20:28:30 -0400

    router

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
+import { HashRouter as Router} from 'react-router-dom';
 import { createStore } from 'redux';
 import rootReducer  from './reducers';
 import './index.css';
@@ -11,7 +12,9 @@ const store = createStore(rootReducer);
 
 ReactDOM.render(
   <Provider store={store}>
-    <App />
+    <Router>
+      <App />
+    </Router>
   </Provider>, 
   document.getElementById('root')
 );
